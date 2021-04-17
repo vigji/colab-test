002a879ae4e1e3ac99ace8c09f7ff70fc7f60bcd
Luigi Petrucco <luigi.petrucco@gmail.com>
2021-04-17 16:30:08 +0200

    Two assignments

--- notebook_test.py
+++
@@ -1,8 +0,0 @@
-import import_ipynb
-import nb_gen
-import pytest
-
-# Test function on some values
-@pytest.mark.parametrize(["in_val", "out_val"], [(0, 1), (4, 24), (1, 1)])
-def test_function(in_val, out_val):
-    assert nb_gen.factorial(in_val) == out_val
---
+++ submission_test.py
@@ -0,0 +1,14 @@
+import import_ipynb
+import nb_gen
+import pytest
+
+# Test hello world function
+@pytest.mark.hallo_world
+def test_hello_world():
+    assert nb_gen.hello_world() == "Hello, world!"
+
+# Test factorial function
+@pytest.mark.factorial
+@pytest.mark.parametrize(["in_val", "out_val"], [(0, 1), (4, 24), (10, 3628800)])
+def test_factorial(in_val, out_val):
+    assert nb_gen.factorial(in_val) == out_val
