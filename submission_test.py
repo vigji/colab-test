b429cbaed5e5bfacd0a869fe3d3afaa316404e9f
Luigi Petrucco <luigi.petrucco@gmail.com>
2021-04-17 16:41:25 +0200

    test fix

--- submission_test.py
+++ submission_test.py
@@ -1,14 +1,14 @@
 import import_ipynb
-import nb_gen
+import submission
 import pytest
 
 # Test hello world function
 @pytest.mark.hello_world
 def test_hello_world():
-    assert nb_gen.hello_world() == "Hello, world!"
+    assert submission.hello_world() == "Hello, world!"
 
 # Test factorial function
 @pytest.mark.factorial
 @pytest.mark.parametrize(["in_val", "out_val"], [(0, 1), (4, 24), (10, 3628800)])
 def test_factorial(in_val, out_val):
-    assert nb_gen.factorial(in_val) == out_val
+    assert submission.factorial(in_val) == out_val
